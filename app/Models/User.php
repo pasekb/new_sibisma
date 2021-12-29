ba66be417110dd1bdb4773ce0880593485ae9c61
Andika Pranayoga <praandikayoga@gmail.com>
2021-12-29 22:49:00 +0800

    Install Jetstream and Livewire

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,35 +6,31 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
-use App\Models\Dealer;
-use App\Models\Unit;
-use App\Models\Color;
-use App\Models\Entry;
-use App\Models\Sale;
-use App\Models\Leasing;
-use App\Models\SaleDelivery;
-use App\Models\Manpower;
-use App\Models\BranchDelivery;
-use App\Models\Opname;
-use App\Models\Document;
-use App\Models\StockHistory;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use HasTeams;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var string[]
      */
     protected $fillable = [
         'name',
-        'dealer_code',
         'email',
-        'username',
         'password',
+        'dealer_code',
+        'username',
         'access',
         'status',
     ];
@@ -42,20 +38,31 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
     ];
 
     /**
      * The attributes that should be cast.
      *
-     * @var array<string, string>
+     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'profile_photo_url',
     ];
 
     // Relasi to created, updated Dealer
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,8 @@ class CreateUsersTable extends Migration
             $table->string('access');
             $table->enum('status',['active','inactive']);
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
     }
