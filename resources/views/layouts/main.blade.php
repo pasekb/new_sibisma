e672f717a0201f6e9d5ac9be65d5b6596cc7f790
Andika Pranayoga <praandikayoga@gmail.com>
2021-12-31 14:07:56 +0800

    Update main.blade, Next edit header.blade livewire

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,35 +13,63 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        @stack('before-css')
+        <!-- Fonts and icons -->
+        <script src="{{ asset('assets/js/plugin/webfont/webfont.min.js') }}"></script>
+        <script>
+            WebFont.load({
+                google: {"families":["Lato:300,400,700,900"]},
+                custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['../assets/css/fonts.min.css']},
+                active: function() {
+                    sessionStorage.fonts = true;
+                }
+            });
+        </script>
+
+        <!-- CSS Files -->
+        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/atlantis.min.css') }}">
+
+        @stack('after-css')
 
         @livewireStyles
-
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
-
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+    <body>
+        <div class="wrapper">
+            <!-- Header -->
+            <livewire:header>
         </div>
 
-        @stack('modals')
+        @stack('before-script')
+        <!-- Script -->
+        <!--   Core JS Files   -->
+        <script src="{{ asset('assets/js/core/jquery.3.2.1.min.js') }}"></script>
+        <script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
+        <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
+
+        <!-- jQuery UI -->
+        <script src="{{ asset('assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js') }}"></script>
+        <script src="{{ asset('assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js') }}"></script>
+
+        <!-- jQuery Scrollbar -->
+	    <script src="{{ asset('assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js') }}"></script>
+
+        <!-- Datatables -->
+        <script src="{{ asset('assets/js/plugin/datatables/datatables.min.js') }}"></script>
+
+        <!-- Bootstrap Notify -->
+        <script src="{{ asset('assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}"></script>
+
+        <!-- jQuery Vector Maps -->
+        <script src="{{ asset('assets/js/plugin/jqvmap/jquery.vmap.min.js') }}"></script>
+        <script src="{{ asset('assets/js/plugin/jqvmap/maps/jquery.vmap.world.js') }}"></script>
+
+        <!-- Sweet Alert -->
+        <script src="{{ asset('assets/js/plugin/sweetalert/sweetalert.min.js') }}"></script>
+
+        <!-- Atlantis JS -->
+        <script src="{{ asset('assets/js/atlantis.min.js') }}"></script>
+        @stack('after-script')
 
         @livewireScripts
     </body>
