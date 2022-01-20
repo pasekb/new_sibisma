d25be5a30e2cfbd1e2b938d8dc91894617998647
Andika Pranayoga <praandikayoga@gmail.com>
2022-01-20 16:32:04 +0800

    Create PSI Chart

---
+++ app/Http/Livewire/PsiChart.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PsiChart extends Component
+{
+    public function render()
+    {
+        return view('livewire.psi-chart');
+    }
+}
