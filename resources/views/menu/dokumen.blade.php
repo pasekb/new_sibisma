4c336d75ad22d08f7b6c089bd6014141a6a067e3
Andika Pranayoga <praandikayoga@gmail.com>
2022-01-31 16:27:47 +0800

    Update menu document

--- resources/views/menu/dokumen.blade.php
+++ resources/views/menu/dokumen.blade.php
@@ -1,6 +1,6 @@
 <li class="nav-item {{ Route::is('document.index') || Route::is('document.show') || Route::is('document.edit') ? 'active' : '' }}" @if(Auth::user()->crud == 'simple') hidden @endif>
     <a href="{{ route('document.index') }}">
         <i class="fas fa-book"></i>
-        <p>Dokumen</p>
+        <p>Document</p>
     </a>
 </li>
